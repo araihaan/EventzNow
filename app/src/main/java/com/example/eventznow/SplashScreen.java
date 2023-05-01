4edf1555fd432116eb512624e786af008ec19698
RaihanZzz <araihanassyam@gmail.com>
2023-05-01 20:17:26 +0800

    start

---
+++ app/src/main/java/com/example/eventznow/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.eventznow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), ActivityWelcome.class);
+                startActivity(i);
+
+                // Menutup activity splash screen
+                finish();
+            }
+        }, 3000);
+    }
+}
