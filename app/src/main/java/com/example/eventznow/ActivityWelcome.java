83d2d1ca0ba1920af4f0f2d2fd272fbf0e43e16a
RaihanZzz <araihanassyam@gmail.com>
2023-05-02 04:05:33 +0800

    new

--- app/src/main/java/com/example/eventznow/ActivityWelcome.java
+++ app/src/main/java/com/example/eventznow/ActivityWelcome.java
@@ -2,7 +2,10 @@ package com.example.eventznow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityWelcome extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class ActivityWelcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button button = findViewById(R.id.btMemberLogin);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityWelcome.this,MemberLogin.class);
+                startActivity(intent);
+
+
+    }
+        });
     }
 }
--- app/src/main/java/com/example/eventznow/MemberRegister.java
+++ app/src/main/java/com/example/eventznow/MemberRegister.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class member_register extends AppCompatActivity {
+public class MemberRegister extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
