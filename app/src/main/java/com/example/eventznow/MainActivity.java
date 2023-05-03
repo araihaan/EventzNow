1c470a293e0ac8b1fc907b3201d4191b796c7a4d
13ViperX <ahmd13fuad@gmail.com>
2023-05-03 23:43:05 +0800

    update

--- app/src/main/java/com/example/eventznow/MainActivity.java
+++ app/src/main/java/com/example/eventznow/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/eventznow/MemberMenu.java
+++ app/src/main/java/com/example/eventznow/MemberMenu.java
@@ -1,22 +1,61 @@
 package com.example.eventznow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MemberMenu extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MemberMenu extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_menu);
 
+        replaceFragment(new HomeFragment());
 
+        BottomNavigationView navigationView = findViewById(R.id.bottomNavigationView);
+        navigationView.setOnItemSelectedListener(this);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                replaceFragment(new HomeFragment());
+                break;
+            case R.id.profil:
+                replaceFragment(new ProfilFragment());
+                break;
+            case R.id.ticket:
+                replaceFragment(new TicketFragment());
+                break;
+        }
+
+        return true;
 
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
     @Override
     public void onBackPressed() {
         finish();
-        }
+    }
 }
