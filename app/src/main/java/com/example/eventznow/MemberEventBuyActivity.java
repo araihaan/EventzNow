588dab69debfed83c22accde34130cedac0f2ad0
RaihanZzz <araihanassyam@gmail.com>
2023-05-21 11:56:21 +0800

    CheckPoint by HanzUwU

--- app/src/main/java/com/example/eventznow/MemberEventBuyActivity.java
+++ app/src/main/java/com/example/eventznow/MemberEventBuyActivity.java
@@ -14,14 +14,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.Locale;
+import java.util.UUID;
 
 public class MemberEventBuyActivity extends AppCompatActivity {
 
-    private TextView eventName, eventDate, eventTime, eventLocation, eventSlot, eventPrice, eventPriceDetail, amountString, totalpaymentString;
-    private Button paymentMethod, addAmount, removeAmount;
+    private TextView eventName, eventDate, eventTime, eventLocation, eventSlot, eventPrice, eventPriceDetail;
+    private Button paymentMethod;
     private String eventID;
-    private int amount = 1;
     private DatabaseReference eventsRef;
 
     @Override
@@ -39,10 +38,7 @@ public class MemberEventBuyActivity extends AppCompatActivity {
         eventSlot = findViewById(R.id.txtSlotString);
         eventPrice = findViewById(R.id.txtPriceString);
         eventPriceDetail = findViewById(R.id.txtPriceDetailString);
-        amountString = findViewById(R.id.txtAmountString);
-        totalpaymentString = findViewById(R.id.txtTotalpaymentDetailString);
-        addAmount = findViewById(R.id.btAddAmount);
-        removeAmount = findViewById(R.id.btRemoveAmount);
+        paymentMethod = findViewById(R.id.btPaymentMethod);
 
         eventsRef = FirebaseDatabase.getInstance().getReference().child("events").child(eventID);
 
@@ -60,8 +56,6 @@ public class MemberEventBuyActivity extends AppCompatActivity {
                         eventSlot.setText(event.getSlot());
                         eventPrice.setText(event.getPrice());
                         eventPriceDetail.setText(event.getPrice());
-
-                        updateTotalPayment();
                     }
                 }
             }
@@ -72,46 +66,19 @@ public class MemberEventBuyActivity extends AppCompatActivity {
             }
         });
 
-        addAmount.setOnClickListener(new View.OnClickListener() {
+        paymentMethod.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                amount++;
-                updateAmountString();
-                updateTotalPayment();
-            }
-        });
+            public void onClick(View v) {
+                // Generate unique order ID
+                String orderId = generateOrderID();
 
-        removeAmount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (amount > 1) {
-                    amount--;
-                    updateAmountString();
-                    updateTotalPayment();
-                }
             }
         });
     }
 
-    private void updateAmountString() {
-        amountString.setText(String.valueOf(amount));
-    }
-
-    private void updateTotalPayment() {
-        String priceString = eventPrice.getText().toString();
-        String numericPriceString = priceString.replaceAll("[^\\d.]", ""); // Remove non-numeric characters
-
-        if (!numericPriceString.isEmpty()) {
-            double price = Double.parseDouble(numericPriceString);
-            double totalPayment = price * amount;
-            String formattedTotalPayment = formatTotalPayment(totalPayment); // Format the total payment
-            totalpaymentString.setText(formattedTotalPayment);
-        }
-    }
-
-    private String formatTotalPayment(double totalPayment) {
-        // Add comma separator for thousands
-        String formattedTotalPayment = String.format(Locale.US, "%,.0f", totalPayment);
-        return formattedTotalPayment;
+    private String generateOrderID() {
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        String uniqueID = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 8);
+        return timestamp + uniqueID;
     }
 }
